06c3fe3c55e0d8b87391d4ed4597707a7951c259
zhouwei <13522569562@163.com>
2018-06-03 11:22:09 +0800

    11

--- spring-cloud-zuul-server/src/main/java/com/huatu/spring/cloud/config/GatewayZuulFallback.java
+++ spring-cloud-zuul-server/src/main/java/com/huatu/spring/cloud/config/GatewayZuulFallback.java
@@ -13,32 +13,41 @@ import java.io.InputStream;
 
 /**
  * @author zhouwei
- * @Description: TODO
+ * @Description: 路由发起请求失败时的回滚处理
  * @create 2018-05-31 下午1:50
  **/
 @Slf4j
 @Component
 public class GatewayZuulFallback implements ZuulFallbackProvider {
+    /**
+     * 需要所有调用都支持回退，则return "*"或return null
+     * @return
+     */
     @Override
     public String getRoute() {
-        return "user-service";//指定回退的服务名
+        return "*";
     }
-
+    /**
+     * 如果请求用户服务失败，返回什么信息给消费者客户端
+     */
     @Override
     public ClientHttpResponse fallbackResponse() {
         return new ClientHttpResponse() {
             @Override
             public HttpStatus getStatusCode() throws IOException {
+                log.error("-----------getStatusCode------------");
                 return HttpStatus.INTERNAL_SERVER_ERROR;
             }
 
             @Override
             public int getRawStatusCode() throws IOException {
+                log.error("-----------getRawStatusCode------------");
                 return this.getStatusCode().value();// 500
             }
 
             @Override
             public String getStatusText() throws IOException {
+                log.error("-----------getStatusText------------");
                 return this.getStatusCode().getReasonPhrase();//  Internal Server Error
             }
 
@@ -48,6 +57,7 @@ public class GatewayZuulFallback implements ZuulFallbackProvider {
 
             @Override
             public InputStream getBody() throws IOException {
+                log.error("-----------getBody------------");
                 return new ByteArrayInputStream(getStatusText().getBytes());
             }
 
