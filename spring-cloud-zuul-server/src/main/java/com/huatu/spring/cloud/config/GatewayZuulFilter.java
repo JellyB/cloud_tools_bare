546af7df5bdb070f5097e6d52936433814971661
shanjigang <shanjigangsjg@163.com>
2019-04-16 17:44:29 +0800

    获取试题音频

--- spring-cloud-zuul-server/src/main/java/com/huatu/spring/cloud/config/GatewayZuulFilter.java
+++ spring-cloud-zuul-server/src/main/java/com/huatu/spring/cloud/config/GatewayZuulFilter.java
@@ -85,6 +85,7 @@ public class GatewayZuulFilter extends ZuulFilter {
         whiteUrls.add("/tk/v1/video/shareCount");
         whiteUrls.add("/tk/v1/video/detail/[0-9]+");
         whiteUrls.add("/tk/v1/comment/ms");
+        whiteUrls.add("/tk/v1/question/audio/[0-9]+");
         // 搜索相关
         whiteUrls.add("/s/v1/user/search");
         whiteUrls.add("/s/v1/question/search");
