db0109d5854adc52f4704e6c7fcbf6ca2f8d93ca
Geek-S <geek-s@outlook.com>
2019-10-09 15:05:55 +0800

    add white urls

--- spring-cloud-zuul-server/src/main/java/com/huatu/spring/cloud/config/GatewayZuulFilter.java
+++ spring-cloud-zuul-server/src/main/java/com/huatu/spring/cloud/config/GatewayZuulFilter.java
@@ -103,7 +103,9 @@ public class GatewayZuulFilter extends ZuulFilter {
         whiteUrls.add("/tk/v1/referenceMaterial/categories");
         whiteUrls.add("/tk/v1/referenceMaterial");
         whiteUrls.add("/tk/v1/referenceMaterial/[0-9]+");
-        whiteUrls.add("/tk/v1/referenceMaterial/browseRecord/[0-9]+");
+        whiteUrls.add("/tk/v1/questionTopic/list");
+        whiteUrls.add("/tk/v1/questionTopic/[0-9]+");
+        whiteUrls.add("/tk/v1/questionTopic/question");
         // 搜索相关
         whiteUrls.add("/s/v1/user/search");
         whiteUrls.add("/s/v1/question/search");
