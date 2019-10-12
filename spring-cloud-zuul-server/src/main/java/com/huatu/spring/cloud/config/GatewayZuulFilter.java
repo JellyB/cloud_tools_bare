52b56cb4e38f169a769f2a731ec8708fafa3eaa5
duanxiangchao <duan.xiang.chao@163.com>
2019-10-12 13:32:37 +0800

    我的动态加白名单

--- spring-cloud-zuul-server/src/main/java/com/huatu/spring/cloud/config/GatewayZuulFilter.java
+++ spring-cloud-zuul-server/src/main/java/com/huatu/spring/cloud/config/GatewayZuulFilter.java
@@ -108,6 +108,10 @@ public class GatewayZuulFilter extends ZuulFilter {
         whiteUrls.add("/tk/v1/questionTopic/list");
         whiteUrls.add("/tk/v1/questionTopic/[0-9]+");
         whiteUrls.add("/tk/v1/questionTopic/question");
+        whiteUrls.add("/tk/v1/person/dynamic");
+        whiteUrls.add("/tk/v1/person/comment");
+        whiteUrls.add("/tk/v1/person/like");
+        whiteUrls.add("/tk/v1/person/collection");
         // 搜索相关
         whiteUrls.add("/s/v1/user/search");
         whiteUrls.add("/s/v1/question/search");
